694ec07800a149e2a8202cbac32fa5d2021b2181
dhanashree-patil-pst <dhanashree.patil@positsource.com>
2024-02-05 12:41:18 +0530

    [server][mongo] Added dbname in .env file, added home route and added variables in constructor

--- server/connection/conn.py
+++ server/connection/conn.py
@@ -8,19 +8,24 @@ load_dotenv()
 ## make connection
 class MogoConnection():
     def __init__(self):
-        pass
-
-    def get_conn():        
-        URI=os.getenv('URI')
-        MONGO_USERNAME=os.getenv('MONGO_USERNAME')
-        MONGO_PASSWORD=os.getenv('MONGO_PASSWORD')
-
-        client=MongoClient(URI)
+        # Load environment variables
+        self.URI = os.getenv('URI')
+        self.MONGO_USERNAME = os.getenv('MONGO_USERNAME')
+        self.MONGO_PASSWORD = os.getenv('MONGO_PASSWORD')
+        self.DBNAME = os.getenv('DBNAME')
+        
+        # Initialize the MongoDB client
+        self.client = MongoClient(self.URI)
 
         ## confirm db connection
         try:   
+            self.client.admin.command('ping')
             print("Pinged your deployment. You successfully connected to MongoDB!")
    
         except Exception as e:
-            print(e)
+            print(f"Error occurred while getting connection from mongo client. \n Error: {e}")
+
+    def get_conn(self):        
+        return self.client
+       
 
--- server/main.py
+++ server/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
-from connection.conn import db
+
 
 app = FastAPI()
 
+class MyApp:
+    # Home route
+    @app.get("/")
+    async def read_root():
+        return {"message": "Welcome to my FastAPI application!"}
 
+
+
+if __name__ == "__main__":
+    my_app = MyApp()
+    my_app.read_root()
