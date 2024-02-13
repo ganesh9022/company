08d11928e57f5dc2424ca06e72fdec1b12243c3b
dhanashree-patil-pst <dhanashree.patil@positsource.com>
2024-02-13 12:25:02 +0530

    Added database creation and collection creation script in function, used proper naming convention

--- server/connection/scripts/migration.py
+++ server/connection/scripts/migration.py
@@ -1,89 +1,138 @@
-from pymongo.mongo_client import MongoClient
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
 from datetime import datetime
 
+
+## load the .env
+load_dotenv()
+
+
 class MongoDBHandler:
-    def __init__(self, username, password, db_name):
-        self.username = username
-        self.password = password
-        self.db_name = db_name
+    def __init__(self): 
+        self.uri = os.getenv('URI')
+        self.db_name = os.getenv('DB_NAME', 'leave_portal') # default one is leave portal
+
+        ## Runtime
         self.client = None
         self.db = None
 
-        self.connect()
-
-    def connect(self):
-        uri = f"mongodb+srv://{self.username}:{self.password}@leaveportal.f5oce1v.mongodb.net/?retryWrites=true&w=majority"
+    #Connection
+    def get_conn(self):
         try:
-            self.client = MongoClient(uri)
+            self.client = MongoClient(self.uri)
             self.client.admin.command('ping')
-            print("Pinged your deployment. You successfully connected to MongoDB!")
-            self.db = self.client[self.db_name]
+            print("Pinged your deployment. You have successfully connected to MongoDB!")
         except Exception as e:
-            print(e)
-
-    def check_database(self):
+            print(f"Error occurred while getting connection from mongo client. \n Error: {e}")
+        return self.client
+    
+    #Database creation
+    def get_db(self,db_name):
         databases = self.client.list_database_names()
-        if self.db_name in databases:
-            print("Database Exists")
+        if db_name in databases:                   #Check if db already exits
+            self.db=self.client[db_name]
+            return self.db
         else:
-            print("Create Database")
-            # You may choose to create the database here
+            try:
+                self.db = self.client[db_name]   
+            except Exception as e:
+                return f"Error occurred while creating database {db_name} : {e}"
+            return self.db
 
+    #Collection creation
     def create_collections(self):
-        # Collection user
-        mycol = self.db["user"]
-        data = {"user_id": "123", "firstname": "Mihir","lastname":"Vanave",
-            "gender":"Male","email":"bapuvanave65@gmail.com",
-            "password":"Mihir@27","role":"Fullstack developer",
-            "department":"","date_of_joining":"","designation":"",
-            "account_number":"","bank_name":"","pan_number":"","country_code":"",
-            "date_of_birth":""}
-        mycol.insert_one(data)
+        try:
+            schema_documents= {
+                # Collection user
+                "user_collection" : {
+                    "user_id": str,
+                    "firstname": str,
+                    "lastname": str,
+                    "gender": str,
+                    "email": str,
+                    "password": str,
+                    "role": str,
+                    "department": str,
+                    "date_of_joining": datetime,
+                    "designation": str,
+                    "account_number": str,
+                    "bank_name": str,
+                    "pan_number": str,
+                    "country_code": str,
+                    "date_of_birth": datetime
+                },
 
-        # Collection Department
-        mycol1 = self.db["Departments"]
-        data1 = {"department": "", "manager": "", "user_id": "", "email": ""}
-        mycol1.insert_one(data1)
+                # Collection Department
+                "department_collection" : {
+                    "department": str,
+                    "manager": str,
+                    "user_id": str,
+                    "email": str
+                },
 
-        # Collection leave_info
-        mycol2 = self.db["leave_info"]
-        data2 = {"user_id": "", "department": "", "total_leaves": "",
-                 "taken_leaves": "", "leave_balance": "",
-                 "leave_type": "", "start_date": "", "end_date": ""}
-        mycol2.insert_one(data2)
+                # Collection leave_info
+                "leave_info_collection" : {
+                    "user_id": str,
+                    "department": str,
+                    "total_leaves": int,
+                    "taken_leaves": int,
+                    "leave_balance": int,
+                    "leave_type": str,
+                    "start_date": datetime,
+                    "end_date": datetime
+                },
 
-        # Collection Reaction
-        mycol3 = self.db["Reaction"]
-        data3 = {"user_id": "", "firstname": "", "lastname": "", "email": ""}
-        mycol3.insert_one(data3)
+                # Collection Reaction
+                "reaction_collection" : {
+                    "user_id": str,
+                    "firstname": str,
+                    "lastname": str,
+                    "email": str
+                },
 
-        # Collection Data
-        mycol4 = self.db["Data"]
-        data4 = {"user_id": "", "firstname": "", "lastname": "", "email": ""}
-        mycol4.insert_one(data4)
+                # Collection Data
+                "data_collection" : {
+                    "user_id": str,
+                    "firstname": str,
+                    "lastname": str,
+                    "email": str
+                },
 
-        # Collection Automation
-        mycol5 = self.db["Automation"]
-        data5 = {"user_id": "", "firstname": "", "lastname": "", "email": ""}
-        mycol5.insert_one(data5)
+                # Collection Automation
+                "automation_collection" : {
+                    "user_id": str,
+                    "firstname": str,
+                    "lastname": str,
+                    "email": str
+                },
 
-        # Collection payslip
-        mycol6 = self.db["payslip"]
-        data6 = {"user_id": "", "allowance(HRA)": "",
-                 "bonus": "", "pf": "", "professional_tax": "",
-                 "present_days": "", "total_salary": ""}
-        mycol6.insert_one(data6)
+                # Collection payslip
+                "payslip_" : {
+                    "user_id": str,
+                    "allowance(HRA)": float,
+                    "bonus": float,
+                    "pf": float,
+                    "professional_tax": float,
+                    "present_days": int,
+                    "total_salary": float
+                }
+                }
 
-    def list_collections(self):
-        print(self.db.list_collection_names())
+            for collection_name, schema in schema_documents.items():
+                try:
+                    collections=self.db.list_collection_names()
+                    if collection_name not in collections:
+                        collection = self.db[collection_name]
+                        collection.insert_many([schema])
+                        return {"message: " f"Inserted  document into collection '{collection_name}'"}
+                    else:
+                        return {"message: " f"collection '{collection_name}' already exists"}
+                except Exception as e:
+                    print(f"Error occurred while inserting  document into collection '{collection_name}': {e}")
+                  
 
-# Example usage:
-if __name__ == "__main__":
-    username = "leave"
-    password = "Vanave2003"
-    db_name = "leaveportal"
+        except Exception as e:
+            print(f"Error occurred while creating collections: {e}")
+            raise
 
-    mongo_handler = MongoDBHandler(username, password, db_name)
-    mongo_handler.check_database()
-    mongo_handler.create_collections()
-    mongo_handler.list_collections()
