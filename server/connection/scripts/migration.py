728a4357e3ca35da26698d36465c5967ba403319
dhanashree-patil-pst <dhanashree.patil@positsource.com>
2024-02-14 13:04:13 +0530

    Created schema file and Updated migration file by adding creat_collection method

--- server/connection/scripts/migration.py
+++ server/connection/scripts/migration.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
-from datetime import datetime
+import json
 
 
 ## load the .env
@@ -40,99 +40,37 @@ class MongoDBHandler:
                 return f"Error occurred while creating database {db_name} : {e}"
             return self.db
 
-    #Collection creation
-    def create_collections(self):
+    
+    #List collections
+    def list_collections(self):
         try:
-            schema_documents= {
-                # Collection user
-                "user_collection" : {
-                    "user_id": str,
-                    "firstname": str,
-                    "lastname": str,
-                    "gender": str,
-                    "email": str,
-                    "password": str,
-                    "role": str,
-                    "department": str,
-                    "date_of_joining": datetime,
-                    "designation": str,
-                    "account_number": str,
-                    "bank_name": str,
-                    "pan_number": str,
-                    "country_code": str,
-                    "date_of_birth": datetime
-                },
+            collection_list= self.db.list_collection_names()
+            return collection_list
+        except Exception as e:
+            return {"message" : f"Error occurred while listing collections: {e}"}
 
-                # Collection Department
-                "department_collection" : {
-                    "department": str,
-                    "manager": str,
-                    "user_id": str,
-                    "email": str
-                },
-
-                # Collection leave_info
-                "leave_info_collection" : {
-                    "user_id": str,
-                    "department": str,
-                    "total_leaves": int,
-                    "taken_leaves": int,
-                    "leave_balance": int,
-                    "leave_type": str,
-                    "start_date": datetime,
-                    "end_date": datetime
-                },
-
-                # Collection Reaction
-                "reaction_collection" : {
-                    "user_id": str,
-                    "firstname": str,
-                    "lastname": str,
-                    "email": str
-                },
-
-                # Collection Data
-                "data_collection" : {
-                    "user_id": str,
-                    "firstname": str,
-                    "lastname": str,
-                    "email": str
-                },
-
-                # Collection Automation
-                "automation_collection" : {
-                    "user_id": str,
-                    "firstname": str,
-                    "lastname": str,
-                    "email": str
-                },
-
-                # Collection payslip
-                "payslip_" : {
-                    "user_id": str,
-                    "allowance(HRA)": float,
-                    "bonus": float,
-                    "pf": float,
-                    "professional_tax": float,
-                    "present_days": int,
-                    "total_salary": float
-                }
-                }
-
-            for collection_name, schema in schema_documents.items():
+    def create_collections(self,schema_file):       
+            with open(schema_file, 'r') as f:
+                collection_schema=json.load(f)
+            for collection_name, schema in collection_schema.items():
                 try:
-                    collections=self.db.list_collection_names()
+                    collections=self.list_collections()
                     if collection_name not in collections:
-                        collection = self.db[collection_name]
-                        collection.insert_many([schema])
-                        return {"message: " f"Inserted  document into collection '{collection_name}'"}
+                        collection = self.db.create_collection(collection_name)
+                        
+                        return {"message: " f"Created collection '{collection_name}'"}
                     else:
                         return {"message: " f"collection '{collection_name}' already exists"}
                 except Exception as e:
-                    print(f"Error occurred while inserting  document into collection '{collection_name}': {e}")
+                    print(f"Error occurred while creating collection '{collection_name}': {e}")
                   
 
-        except Exception as e:
-            print(f"Error occurred while creating collections: {e}")
-            raise
+       
 
+
+if __name__ == "__main__":
+    mongo_handler = MongoDBHandler()
+    mongo_handler.get_conn()
+    mongo_handler.get_db(mongo_handler.db_name)
+    mongo_handler.create_collections('schema.json')
+    mongo_handler.list_collections()
